f6f5adc3745eb495148cd964b8cf0032b1807c39
Shrutimishraa <shruti19122001@gmail.com>
2024-06-08 20:50:20 +0530

    Implement Player model

---
+++ src/entities/player.js
@@ -0,0 +1,13 @@
+class Player {
+    constructor( name, health, strength, attack ){
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+
+    }
+    isAlive(){
+        return this.health > 0;
+    }
+}
+module.exports = Player;
